fa0f6beff9d8db4f10112a57b4d61939ba6d4a74
Kojima Kohei <125636684+k-kojima-yumemi@users.noreply.github.com>
2023-05-25 15:27:18 +0900

    Add other case

--- test/test_openapi_schema.py
+++ test/test_openapi_schema.py
@@ -1,6 +1,7 @@
 import json
 from pathlib import Path
 
+import pytest
 import yaml
 
 import openapi_main
@@ -8,6 +9,7 @@ import openapi_main
 resource_parent = Path("resources")
 schema_path = resource_parent / Path("openapi/openapi.yaml")
 content_success_path = resource_parent / Path("json/test-openapi.json")
+content_success_path2 = resource_parent / Path("json/test-openapi-lunch.json")
 content_fail_path = resource_parent / Path("json/test-region-no-area.json")
 content_empty_path = resource_parent / Path("json/empty.json")
 
@@ -16,6 +18,7 @@ def test_check_file() -> None:
     files = [
         schema_path,
         content_success_path,
+        content_success_path2,
         content_fail_path,
         content_empty_path,
     ]
@@ -24,9 +27,13 @@ def test_check_file() -> None:
     return
 
 
-def test_success() -> None:
-    schema = get_schema(schema_path, "User")
-    content = get_json(content_success_path)
+@pytest.mark.parametrize("json_path, def_name, ", [
+    (content_success_path, "User"),
+    (content_success_path2, "lunch"),
+])
+def test_success(json_path: Path, def_name: str) -> None:
+    schema = get_schema(schema_path, def_name)
+    content = get_json(json_path)
     errors = openapi_main.do_validation(schema, [("", content)])
     assert not errors
     pass
@@ -44,8 +51,12 @@ def test_fail() -> None:
     return
 
 
-def test_fail_empty() -> None:
-    schema = get_schema(schema_path, "User")
+@pytest.mark.parametrize("def_name, ", [
+    "User",
+    "lunch",
+])
+def test_fail_empty(def_name: str) -> None:
+    schema = get_schema(schema_path, def_name)
     path = content_empty_path
     content = get_json(path)
     errors = openapi_main.do_validation(schema, [(str(path), content)])
@@ -53,6 +64,18 @@ def test_fail_empty() -> None:
     assert len(errors) == 1
     file_name, error = errors[0]
     assert file_name == str(path)
+    return
+
+
+@pytest.mark.parametrize("json_path, def_name, ", [
+    (content_success_path, "lunch"),
+    (content_success_path2, "User"),
+])
+def test_fail_wrong_definition(json_path: Path, def_name: str) -> None:
+    schema = get_schema(schema_path, def_name)
+    content = get_json(json_path)
+    errors = openapi_main.do_validation(schema, [(str(json_path), content)])
+    assert errors
     return
 
 
