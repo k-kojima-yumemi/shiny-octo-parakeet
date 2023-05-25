62870a371d9ef04c93461d3b447aea7b7690614a
Kojima Kohei <125636684+k-kojima-yumemi@users.noreply.github.com>
2023-05-25 19:57:21 +0900

    PHP console

--- check_main.php
+++ check_main.php
@@ -4,9 +4,13 @@ declare(strict_types=1);
 
 require_once("vendor/autoload.php");
 
+use Symfony\Component\Console\Application;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Input\InputOption;
+use Symfony\Component\Console\Output\OutputInterface;
 use Symfony\Component\Yaml\Yaml;
 
-function getSchema(string $fileName, ?string $target = null)
+function getSchema(string $fileName, ?string $target): object
 {
     if (str_ends_with($fileName, ".json") || str_ends_with($fileName, ".yaml")) {
         // YAML or JSON file
@@ -19,14 +23,21 @@ function getSchema(string $fileName, ?string $target = null)
     } else {
         throw new AssertionError();
     }
+    return $content;
+}
 
+function validate(object $schema, object $json, string $fileName): array
+{
     $validator = new JsonSchema\Validator();
-    $json = json_decode(file_get_contents("resources/json/test-region-no-area.json"));
-
-    $exitCode = $validator->validate($json, schema: $content);
+    $validator->validate($json, schema: $schema);
     $error = $validator->getErrors();
-    print_r($error);
-    exit($exitCode);
+    if (!empty($error)) {
+        return [
+            $fileName => $error
+        ];
+    } else {
+        return [];
+    }
 }
 
 function array_to_object($array)
@@ -35,4 +46,33 @@ function array_to_object($array)
     return json_decode($json_str);
 }
 
-getSchema("resources/json-schema/regions.json");
+function main(OutputInterface $output, string $schemaFileName, string $jsonFileName, ?string $target = null): int
+{
+    $output->writeln("Start main");
+    $schema = getSchema($schemaFileName, $target);
+    $json = json_decode(file_get_contents($jsonFileName));
+    $error = validate($schema, $json, $jsonFileName);
+    foreach ($error as $e) {
+        $output->writeln(print_r($e, return: true));
+    }
+    $output->writeln("End main");
+    return empty($error) ? 0 : 1;
+}
+
+$application = new Application();
+$application
+    ->register("lint")
+    ->addArgument("schema_file", InputOption::VALUE_REQUIRED)
+    ->addArgument("check_files", InputOption::VALUE_REQUIRED)
+    ->addArgument("target_schema", InputOption::VALUE_OPTIONAL)
+    ->setCode(function (InputInterface $input, OutputInterface $output): int {
+        $target = $input->getArgument("target_schema");
+        return main(
+            output: $output,
+            schemaFileName: $input->getArgument("schema_file"),
+            jsonFileName: $input->getArgument("check_files"),
+            target: empty($target) ? null : $target,
+        );
+    });
+
+$application->run();
